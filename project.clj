28b505e656d0b4df0f404410aac0b632a0b54d84
Yurii <juretz32@yandex.ru>
2025-05-01 10:50:04 +0300

    Bump Selenium version

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :description "Selenium Webdriver wrapper"
   :repositories [["jitpack" "https://jitpack.io"]]
   :dependencies [[org.clojure/clojure "1.12.0"]
-                 [org.seleniumhq.selenium/selenium-java "3.141.59"]
+                 [org.seleniumhq.selenium/selenium-java "4.31.0"]
                  [com.github.detro/ghostdriver "2.1.0"]]
   :repl-options {:init-ns selenium.core})
